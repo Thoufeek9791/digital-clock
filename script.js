cdca5a2f6a417a074357ef2f60d0f188268c6bd9
Thoufeek <thoufeekrahuman9361@gmail.com>
2022-12-15 12:05:12 +0530

    commited

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const clock = document.querySelector(".clock");
+const span = document.querySelectorAll(".blink");
+const hours = document.querySelector(".hrs");
+const minutes = document.querySelector(".mins");
+const seconds = document.querySelector(".sec");
+let isSecondTime = false;
+console.log(hours, minutes, seconds);
+console.log(span);
 console.log(clock);
 console.log(new Date());
 
@@ -14,16 +21,27 @@ function runClock() {
   if (hrs > 12) {
     hrs -= 12;
     txt = "PM";
-  } else if (hrs === 12) {
+  } else if (hrs === 12 && !isSecondTime) {
     hrs = 12;
     txt = "AM";
+    isSecondTime = true;
+  } else if (hrs === 12 && isSecondTime) {
+    hrs = 12;
+    txt = "PM";
+    isSecondTime = false;
   }
+
+  span.innerText = ":";
 
   hrs = hrs < 10 ? "0" + hrs : hrs;
   mins = mins < 10 ? "0" + mins : mins;
   sec = sec < 10 ? "0" + sec : sec;
 
-  clock.innerHTML = `${hrs} : ${mins} : ${sec} ${txt}`;
+  hours.innerText = hrs;
+  minutes.innerText = mins;
+  seconds.innerText = sec;
+
+  // clock.innerHTML = `${hrs}  ${mins}  ${sec} ${txt}`;
 }
 
 window.onload = setInterval(runClock, 1000);
